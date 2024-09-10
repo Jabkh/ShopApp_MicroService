0991161dfebdb4fd09d12772f8ae5b62415a8a09
JaberKh <j.khaloua@hotmail.com>
2024-09-10 10:00:14 +0200

    commit init

---
+++ SecurityHandler/src/main/java/com/example/securityhandler/SecurityHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityHandlerApplication.class, args);
+    }
+
+}
---
+++ SecurityHandler/src/test/java/com/example/securityhandler/SecurityHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
